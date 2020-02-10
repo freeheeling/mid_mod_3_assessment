1370108762cb0478f638745e67ac27a389163548
Scott Regenthal <scottregenthal@gmail.com>
2020-02-10 10:44:56 -0700

    Encapsulate via private method

--- app/poros/house_member_search.rb
+++ app/poros/house_member_search.rb
@@ -9,25 +9,9 @@ class HouseMemberSearch
 
   def members
     service = PotterService.new
-    service.members_by_house.map do |member_data|
-      Member.new(member_data)
-    end
+    members = service.members_by_house(house)
 
-
-    conn = Faraday.new(url: 'https://www.potterapi.com/v1/') do |f|
-      f.request :url_encoded
-      f.adapter Faraday.default_adapter
-      f.params['key'] = ENV['POTTER_API_KEY']
-    end
-
-    response = conn.get('characters') do |req|
-      req.params['orderOfThePhoenix'] = true
-      req.params['house'] = 'Gryffindor'
-    end
-
-    member_search_data = JSON.parse(response.body, symbolize_names: true)
-
-    member_search_data.map do |member_data|
+    members.map do |member_data|
       Member.new(member_data)
     end
   end
--- app/services/potter_service.rb
+++ app/services/potter_service.rb
@@ -1,17 +1,20 @@
 class PotterService
-
   def members_by_house(house)
-    conn = Faraday.new(url: 'https://www.potterapi.com/v1/') do |f|
-      f.request :url_encoded
-      f.adapter Faraday.default_adapter
-      f.params['key'] = ENV['POTTER_API_KEY']
-    end
-
     response = conn.get('characters') do |req|
       req.params['orderOfThePhoenix'] = true
       req.params['house'] = house
     end
 
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://www.potterapi.com/v1/') do |f|
+      f.request :url_encoded
+      f.adapter Faraday.default_adapter
+      f.params['key'] = ENV['POTTER_API_KEY']
+    end
   end
 end
